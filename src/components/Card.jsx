996c411f0b28f5a1288639af8225f36cbcb6b1b9
Capyslaay <tubenparhaat@gmail.com>
2025-12-16 22:26:45 +0200

    fix: Optimize for mobile Safari (viewport, touch actions)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -24,7 +24,7 @@ export default function Card({ politician, onSwipe, style }) {
 
     return (
         <motion.div
-            style={{ x, y, rotate, ...style }}
+            style={{ x, y, rotate, touchAction: 'none', ...style }}
             drag
             dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
             dragElastic={0.7}
